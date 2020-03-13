11ca66a7fad146e66bb2faae98302b3bdc2fa89a
Rafael Santana <raf.santa11@gmail.com>
2020-03-13 13:34:35 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// SIDEBAR
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
+});
+const elemsSidenav = document.querySelectorAll(".sidenav");
+const instancesSidenav = M.Sidenav.init(elemsSidenav, {
+    edge: "left"
+});
