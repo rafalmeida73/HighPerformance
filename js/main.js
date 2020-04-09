8e11b3d0488d002c5b556e5464c5447db075a588
Rafael Santana <raf.santa11@gmail.com>
2020-04-09 14:44:25 -0300

    Adicionando pwa

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ const elemsSidenav = document.querySelectorAll(".sidenav");
 const instancesSidenav = M.Sidenav.init(elemsSidenav, {
     edge: "left"
 });
+
+
+let myPrompt;
+const pwaAlert = document.querySelector('.pwa__alert');
+const btnPwa = document.querySelector('.pwa__alert__btn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+
+    myPrompt = e;
+
+    pwaAlert.style.display = 'block';
+});
+
+btnPwa.addEventListener('click', () => {
+    pwaAlert.style.display = 'none';
+    myPrompt.prompt();
+    myPrompt.userChoice
+        .then((choiceResult) => {
+            if (choiceResult == 'accepted') {
+                console.log('Usuário aceito o prompt');
+            } else {
+                onsole.log('Usuário cancelou o prompt');
+            }
+        });
+});
