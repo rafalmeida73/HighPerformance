020736648de2b22aee5070a64e9759eac32ba6aa
Rafael Santana <raf.santa11@gmail.com>
2020-04-23 21:51:27 -0300

    Arrumando link tr

--- js/exportar.js
+++ js/exportar.js
@@ -24,7 +24,7 @@ $("#csv").click(function() {
 
 // Link para tr
 $(document).ready(function() {
-    $('table').click(function() {
+    $('table tr').click(function() {
         window.location = $(this).data('url');
         returnfalse;
     });
