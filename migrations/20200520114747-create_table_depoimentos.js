1ef802c909b5bf532133baa796e5ecf060d61c06
paulo <pauloernestom@gmail.com>
2020-06-13 14:43:26 -0300

    Ajustando migraiton depoimentos

--- migrations/20200520114747-create_table_depoimentos.js
+++ migrations/20200520114747-create_table_depoimentos.js
@@ -11,19 +11,18 @@ module.exports = {
             autoIncrement: true,
             allowNull:false
         },
-        depoimento: {
-            type: Sequelize.STRING(300),
+        nome: {
+            type: Sequelize.STRING(100),
             allowNull: false
         },
-        treinadores_id:{
-          type:Sequelize.INTEGER,
-          references:{
-            model:'treinadores',
-            key:'id'
-          },
-          onUpdate:'CASCADE',
-          onDelete:'CASCADE'
-        }
+        profissao: {
+            type: Sequelize.STRING(50),
+            allowNull: false
+        },
+        mensagem: {
+          type: Sequelize.STRING(100),
+          allowNull: false
+      },
     });
   },
 
