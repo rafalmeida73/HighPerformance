190a1d4fdf9dbada737353f9caf1e2df8ec8f286
Rafael Santana <rafaelsantana7213@gmail.com>
2020-06-16 01:27:53 -0300

    Arrumando login controller

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -15,47 +15,30 @@ module.exports = {
     //####################################################################################################################################
     showCrie: async(req, res) => {
         let user = req.session.usuario;
-        let aulasArray = []
-        let dt = new Date().toLocaleDateString().split('/').reverse().join('-')
-        let dataInicio = new Date(dt).getTime()
-        let dataFinal  = addDias(dataInicio,10)
+        
 
-        const aulas = await Aula.findAndCountAll({
-            attributes: ['data_aula'],
-            group: ['data_aula'],            
-            where: {
-                treinadores_id: user.id,
-                data_aula:{[Op.between]: ['2020-06-01', '2020-06-24']}
-            },            
-            include: [{
-                    model: Aluno,
-                    as: 'aluno'
-                },
-            ]
-        }).then(aulas => {
-            // criando array com as datas do calendário conforme período dataInicio e dataFinal definidas acima
-            //cont=0
-            while(new Date(dataInicio) <= new Date(dataFinal)) {
-                
-                //aulasArray.push({data:dataInicio,qtde:0})
-                aulasArray.push([dataInicio,0])
-                dataInicio=addDias(dataInicio,1)
-            }
+        let aulas = await Aula.findAndCountAll({
+        //     attributes: ['data_aula'],
+             group: ['data_aula'],            
+             where: {
+                 treinadores_id: user.id
 
+             },
+             raw:true
+         })
+         console.log(aulas)
+         console.log('==================================================================')        
+
+         let periodo = await helpers.periodo()
             // atualizando o array criado acima com as aulas cadastradas no banco EX: 16/6/2020 ---- 1 aula, 21/6/2020 ---- 2 aula
-            for(dia of aulasArray){
-                
-                for(qtde of aulas.count){                    
-                        dia[1] = qtde.count                            
+            for await (dia of periodo){                
+                for(qtde of aulas.count){
+                    if (dia.data === helpers.formatDate(qtde.data_aula) ){
+                        console.log(dia.data,helpers.formatDate(qtde.data_aula), true) 
+                        dia.qtde = await parseInt(qtde.count)
                     }
                 }                   
-            
-            
-            // aulasArray = JSON.parse(aulasArray)
-            // console.log(aulasArray)
-            // console.log('==================================================================')
-            res.render("crie", { user, aulasArray});
-        });
+
 
     },
 
