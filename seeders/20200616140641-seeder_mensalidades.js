bbeda5ebc5b8b80737f377fb8037e970094bcdf5
paulo <pauloernestom@gmail.com>
2020-06-17 13:32:45 -0300

    Ajuste nas datas

--- seeders/20200616140641-seeder_mensalidades.js
+++ seeders/20200616140641-seeder_mensalidades.js
@@ -8,140 +8,140 @@ module.exports = {
         'mensalidades', 
         [
           {
-            mes_ref: 'Janeiro',
+            mes_ref: '01/02/2020',
             valor: 50,
             alunos_id: 1,
             treinadores_id:1,
             status:1
           },
           {
-            mes_ref: 'Fevereiro',
+            mes_ref: '01/03/2020',
             valor: 50,
             alunos_id: 1,
             treinadores_id:1,
             status:1
           },
           {
-            mes_ref: 'Março',
+            mes_ref: '01/04/2020',
             valor: 50,
             alunos_id: 1,
             treinadores_id:1,
             status:1
           },
           {
-            mes_ref: 'Abril',
+            mes_ref: '01/05/2020',
             valor: 50,
             alunos_id: 1,
             treinadores_id:1,
             status:1
           },
           {
-            mes_ref: 'Janeiro',
+            mes_ref: '01/06/2020',
             valor: 50,
             alunos_id: 2,
             treinadores_id:1,
             status:1
           },
           {
-            mes_ref: 'Fevereiro',
+            mes_ref: '01/07/2020',
             valor: 50,
             alunos_id: 2,
             treinadores_id:1,
             status:1
           },
           {
-            mes_ref: 'Março',
+            mes_ref: '01/08/2020',
             valor: 50,
             alunos_id: 2,
             treinadores_id:1,
             status:1
           },
           {
-            mes_ref: 'Abril',
+            mes_ref: '01/02/2020',
             valor: 50,
             alunos_id: 2,
             treinadores_id:1,
             status:0
           },
           {
-            mes_ref: 'Janeiro',
+            mes_ref: '01/02/2020',
             valor: 50,
             alunos_id: 3,
             treinadores_id:2,
             status:0
           },
           {
-            mes_ref: 'Fevereiro',
+            mes_ref: '01/03/2020',
             valor: 50,
             alunos_id: 3,
             treinadores_id:2,
             status:0
           },
           {
-            mes_ref: 'Março',
+            mes_ref: '01/04/2020',
             valor: 50,
             alunos_id: 3,
             treinadores_id:2,
             status:0
           },
           {
-            mes_ref: 'Abril',
+            mes_ref: '01/05/2020',
             valor: 50,
             alunos_id: 3,
             treinadores_id:2,
             status:0
           },
           {
-            mes_ref: 'Janeiro',
+            mes_ref: '01/06/2020',
             valor: 50,
             alunos_id: 4,
             treinadores_id:3,
             status:0
           },
           {
-            mes_ref: 'Fevereiro',
+            mes_ref: '01/07/2020',
             valor: 50,
             alunos_id: 4,
             treinadores_id:3,
             status:0
           },
           {
-            mes_ref: 'Março',
+            mes_ref: '01/08/2020',
             valor: 50,
             alunos_id: 4,
             treinadores_id:3,
             status:0
           },
           {
-            mes_ref: 'Abril',
+            mes_ref: '01/09/2020',
             valor: 50,
             alunos_id: 4,
             treinadores_id:3,
             status:1
           },
           {
-            mes_ref: 'Janeiro',
+            mes_ref: '01/02/2020',
             valor: 50,
             alunos_id: 1,
             treinadores_id:4,
             status:1
           },
           {
-            mes_ref: 'Fevereiro',
+            mes_ref: '01/04/2020',
             valor: 50,
             alunos_id: 1,
             treinadores_id:4,
             status:1
           },
           {
-            mes_ref: 'Março',
+            mes_ref: '01/05/2020',
             valor: 50,
             alunos_id: 1,
             treinadores_id:4,
             status:1
           },
           {
-            mes_ref: 'Abril',
+            mes_ref: '01/06/2020',
             valor: 50,
             alunos_id: 1,
             treinadores_id:4,
