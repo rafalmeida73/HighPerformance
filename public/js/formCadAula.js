243d594f7723e127b814c908749e2cffde824dd8
Rafael Santana <rafaelsantana7213@gmail.com>
2020-06-19 13:32:16 -0300

    Validando forms e alternado css

---
+++ public/js/formCadAula.js
@@ -0,0 +1,34 @@
+$("#formCadAula").validate({
+ rules: {
+     nome: {
+         required: true,
+         maxlength: 100,
+         minlength: 5,
+         minWords: 2
+     },
+     observacoes: {
+      required: true,
+     },
+     alunos_id: {
+         required: true,
+     },
+     data_aula: {
+         required: true,
+         date : true,
+         dateFormat: true
+     },
+     horario: {
+         required: true,
+         time: true
+     }
+ },
+ errorElement: 'div',
+ errorPlacement: function(error, element) {
+     var placement = $(element).data('error');
+     if (placement) {
+         $(placement).append(error)
+     } else {
+         error.insertAfter(element);
+     }
+ }
+});
